45404d426e856d2b381f575d6a0f3d90f97558bb
Tácio Cabral <tacio_cabral@yahoo.com.br>
2019-04-20 14:42:42 -0300

    First Make

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,15 @@
-void kernelMain()
+void printf(*char str)
 {
-    printf("Hello World! --- Tácio Cabral")
+    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+    for (int i = 0; str[i] != '\0'; ++i)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+        
+}
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+{
+    printf("Hello World! --- Tácio Cabral");
+
     while(l)
 }
