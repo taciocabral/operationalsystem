7f2f7e1710fbebf6abf0c4aa027f25f9f4f0d5a0
Tácio Cabral <tacio_cabral@yahoo.com.br>
2019-04-20 15:01:02 -0300

    Corrections

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
-GPPPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-leading-undescore
+GCCPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-leading-undescore
 ASPARAMS = --32
 LDPARAMS = -melf_i386
 
 objects = loader.o kernel.o
 
 %.o: %.cpp
-	g++ $(GPPPARAMS) -o $@ -c %<
+	gcc $(GCCPARAMS) -c -o $@ $<
 
 %.o: %.s
-	sd $(ASPARAMS) -o $@ $<
+	as $(ASPARAMS) -o $@ $<
 
 mykernel.bin: linker.ld $(objects)
 	ld $(LDPARAMS) -T $< -o $@ $(objects)
--- kernel.cpp
+++ kernel.cpp
@@ -1,15 +1,27 @@
-void printf(*char str)
+void printf(char* str)
 {
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    static unsigned short* VideoMemory = (unsigned short*)0xb8000;
 
-    for (int i = 0; str[i] != '\0'; ++i)
+    for(int i = 0; str[i] != '\0'; ++i)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
-        
 }
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
-{
-    printf("Hello World! --- Tácio Cabral");
 
-    while(l)
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
+}
+
+
+
+extern "C" void kernelMain(const void* multiboot_structure, unsigned int /*multiboot_magic*/)
+{
+    printf("Hello World! --- http://www.AlgorithMan.de");
+
+    while(1);
 }
